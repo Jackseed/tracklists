d2867d72fa6bb95ac7601b66fd7d3a4046555ba8
Jackseed <vinkho.c@gmail.com>
2020-11-28 01:30:03 +0100

    track view

--- src/app/tracks/track-view/track-view.component.ts
+++ src/app/tracks/track-view/track-view.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Track } from '../+state';
 
 @Component({
   selector: 'app-track-view',
   templateUrl: './track-view.component.html',
-  styleUrls: ['./track-view.component.css']
+  styleUrls: ['./track-view.component.css'],
 })
 export class TrackViewComponent implements OnInit {
+  @Input() track: Track;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/tracks/tracks.module.ts
+++ src/app/tracks/tracks.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TrackListComponent } from './track-list/track-list.component';
 import { TrackViewComponent } from './track-view/track-view.component';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [TrackListComponent, TrackViewComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule],
   exports: [TrackListComponent, TrackViewComponent],
 })
 export class TracksModule {}
