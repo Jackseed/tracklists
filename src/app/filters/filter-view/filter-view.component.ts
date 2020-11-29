fd9923478b18f4bf8c61c2b0e9bd14877ae2c3ee
Jackseed <vinkho.c@gmail.com>
2020-11-29 17:53:21 +0100

    range filter working example

--- src/app/filters/filter-view/filter-view.component.ts
+++ src/app/filters/filter-view/filter-view.component.ts
@@ -1,27 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { TrackService } from 'src/app/tracks/+state';
 
-@UntilDestroy()
 @Component({
   selector: 'app-filter-view',
   templateUrl: './filter-view.component.html',
   styleUrls: ['./filter-view.component.css'],
 })
 export class FilterViewComponent implements OnInit {
-  public filter = new FormControl();
-  rangeValues: number[];
+  rangeValues: number[] = [0, 0];
 
   constructor(private trackService: TrackService) {}
 
-  ngOnInit(): void {
-    this.filter.valueChanges.pipe(untilDestroyed(this)).subscribe((energy) => {
-      this.trackService.setFilter({
-        id: 'energy',
-        value: energy,
-        predicate: (entity) => entity.energy === energy,
-      });
+  ngOnInit(): void {}
+
+  public onChange() {
+    this.trackService.setFilter({
+      id: 'energy',
+      value: this.rangeValues,
+      predicate: (entity) =>
+        this.rangeValues[0] < entity.energy &&
+        entity.energy < this.rangeValues[1],
     });
   }
 }
