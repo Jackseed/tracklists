3a53e4182f172da177ccf6f1aec755e228265936
Jackseed <vinkho.c@gmail.com>
2021-02-21 11:16:01 +0100

    connects to spotify

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 const functions = require('firebase-functions');
 
-exports.scrapeImages = functions
+exports.scrapeNova = functions
   .runWith({
     timeoutSeconds: 500,
   })
@@ -9,7 +9,7 @@ exports.scrapeImages = functions
     const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
 
-    // mock CET
+    // mock CET time
     await page.emulateTimezone('Europe/Brussels');
     // go to Nova
     await page.goto('https://www.nova.fr/c-etait-quoi-ce-titre/');
@@ -17,7 +17,6 @@ exports.scrapeImages = functions
     await page.click('[id="didomi-notice-agree-button"]');
     // get spotify links
     const data = await page.evaluate(async () => {
-      // TODO: add click on load more first
       const links = document.querySelectorAll('a');
       const urls = Array.from(links)
         .map((link) => link.href)
@@ -25,6 +24,37 @@ exports.scrapeImages = functions
 
       return urls;
     });
+
+    /// SPOTIFY
+    const scope = ['playlist-modify-public', 'playlist-modify-private'].join(
+      '%20'
+    );
+    // authentication url
+    const authorizeURL =
+      'https://accounts.spotify.com/authorize' +
+      '?' +
+      'client_id=' +
+      functions.config().spotify.clientid +
+      '&response_type=' +
+      functions.config().spotify.responsetype +
+      '&redirect_uri=' +
+      functions.config().spotify.redirecturi +
+      '&scope=' +
+      scope;
+
+    console.log(authorizeURL);
+
+    await page.goto(authorizeURL);
+
+    await page.type('[name=username]', functions.config().spotify.email);
+
+    await page.type('[name=password]', functions.config().spotify.password);
+
+    await page.screenshot({ path: '1.png' });
+
+    await page.click('#login-button');
+    await page.waitForTimeout(5000);
+    await page.screenshot({ path: '4.png' });
 
     await browser.close();
 
