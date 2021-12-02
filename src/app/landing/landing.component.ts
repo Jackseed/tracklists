07a4f6eec2d55b7e028d3512f9bc9bc698c4ce63
Jackseed <vinkho.c@gmail.com>
2021-12-02 18:33:13 +0100

    animations

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -2,13 +2,26 @@ import { Component, OnInit } from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
 import { Router } from '@angular/router';
+import {
+  heartBeatAnimation,
+  pulseAnimation,
+  rotateAnimation,
+} from 'angular-animations';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.css'],
+  animations: [
+    pulseAnimation({ duration: 3000, scale: 1.1 }),
+    heartBeatAnimation({ duration: 2000, scale: 1.1 }),
+    rotateAnimation({ duration: 2000 }),
+  ],
 })
 export class LandingComponent implements OnInit {
+  animating = [false, false, false];
+  animStates = [false, false, false];
+
   constructor(
     private matIconRegistry: MatIconRegistry,
     private domSanitizer: DomSanitizer,
@@ -26,5 +39,20 @@ export class LandingComponent implements OnInit {
 
   navigateToLogin() {
     this.router.navigate(['login']);
+  }
+
+  toggleAnimation(animNumber: number) {
+    this.animating[animNumber] = !this.animating[animNumber];
+    this.animStates[animNumber] = !this.animStates[animNumber];
+  }
+
+  animDone(animNumber: number) {
+    if (this.animating[animNumber]) {
+      this.animStates[animNumber] = !this.animStates[animNumber];
+    }
+  }
+
+  loopDone() {
+    this.animStates[3] = !this.animStates[3];
   }
 }
